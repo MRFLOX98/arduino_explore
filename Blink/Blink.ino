a70394e25b38715ec25e5da44be47e6167aef233
Ломов Илья <mrflox@w12-413-alt-8.alt.sgu.ru>
2026-04-21 23:22:13 +0400

    Second commit

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,11 +1,22 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
+  pinMode(3, OUTPUT);
   pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(150);                       // wait for a second
-  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
-  delay(150);                       // wait for a second
+  for(int i=3;i<12;i++)
+  {
+    if(i==3||i==5||i==6||i==9||i==10||i==11)
+    {
+      digitalWrite(i, HIGH);  
+      delay(150);                   
+      digitalWrite(i, LOW);    
+      delay(150);  
+    }
+   }
 }
